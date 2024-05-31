21ecfa63164b65d195078dd0e427d40e0e169100
Ecthor Silva <EcthorSilva@outlook.com>
2024-05-31 20:05:58 -0300

    Feat: alterado o mecanismo de busca para deixa-lo mais otimizado

--- index.js
+++ index.js
@@ -1,118 +1,100 @@
 const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
-const moment = require('moment');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Função para buscar dados do site FitGirl Repacks para uma página específica
 async function fetchFitGirlPage(pageNumber) {
-    try {
-        console.log(`Buscando dados da página ${pageNumber}...`);
-        const response = await axios.get(`https://fitgirl-repacks.site/page/${pageNumber}/`);
-        const html = response.data;
-        const $ = cheerio.load(html);
+   try {
+      console.log(`Buscando dados da página ${pageNumber}...`);
+      const response = await axios.get(`https://fitgirl-repacks.site/page/${pageNumber}/`);
+      const html = response.data;
+      const $ = cheerio.load(html);
 
-        // Array para armazenar os resultados desta página
-        let results = [];
+      // Array para armazenar os resultados desta página
+      let results = [];
 
-        // Buscando posts
-        $('article').each((index, element) => {
-            const title = $(element).find('.entry-title a').text();
-            const link = $(element).find('.entry-title a').attr('href');
-            const magnetLink = $(element).find('a[href^="magnet:?"]').attr('href');
-            const postDate = $(element).find('.entry-date time').attr('datetime');
+      // Buscando posts
+      $('article').each((index, element) => {
+         const title = $(element).find('.entry-title a').text();
+         const link = $(element).find('.entry-title a').attr('href');
+         const magnetLink = $(element).find('a[href^="magnet:?"]').attr('href');
+         const postDate = $(element).find('.entry-date time').attr('datetime');
 
-            // Log dos dados buscados
-            console.log(`Título: ${title}, Link: ${link}, Link Magnet: ${magnetLink}, Data do Post: ${postDate}`);
+         // Incluir posts e adicionar link ao título se todos os dados estiverem presentes
+         if (title && magnetLink && postDate) {
+            results.push({ title, link, magnetLink });
+         }
+      });
 
-            // Incluir posts e adicionar link ao título
-            if (title && magnetLink && postDate) {
-                results.push({ title, link, magnetLink });
-            }
-        });
-
-        console.log(`Resultados da página ${pageNumber}:`, results);
-        return results;
-    } catch (error) {
-        console.error('Erro ao buscar dados da página:', error.message);
-        return [];
-    }
+      return results; // Retorna os resultados da página
+   } catch (error) {
+      console.error('Erro ao buscar dados da página:', error.message);
+      return []; // Retorna um array vazio em caso de erro
+   }
 }
 
 // Função para buscar dados do site FitGirl Repacks
 async function fetchFitGirlData(searchTerm = '') {
-    try {
-        let currentPage = 1;
-        let found = false;
-        let results = [];
+   try {
+      const maxPages = 100; // Limite de 100 páginas
+      let currentPage = 1;
+      let found = false;
+      let results = [];
 
-        while (!found && currentPage <= 50) { // Limitando a 10 páginas
-            console.log(`Procurando na página ${currentPage}...`);
-            const response = await axios.get(`https://fitgirl-repacks.site/page/${currentPage}/`);
-            const html = response.data;
-            const $ = cheerio.load(html);
-
-            // Buscando posts
-            $('article').each((index, element) => {
-                const title = $(element).find('.entry-title a').text();
-                const link = $(element).find('.entry-title a').attr('href');
-                const magnetLink = $(element).find('a[href^="magnet:?"]').attr('href');
-                const postDate = $(element).find('.entry-date time').attr('datetime');
-
-                // Log dos dados buscados
-                console.log(`Título: ${title}, Link: ${link}, Link Magnet: ${magnetLink}, Data do Post: ${postDate}`);
-
-                // Incluir posts e adicionar link ao título
-                if (title && magnetLink && postDate) {
-                    // Verificar se o título contém o termo de busca (case insensitive)
-                    if (title.toLowerCase().includes(searchTerm.toLowerCase())) {
-                        results.push({ title, link, magnetLink });
-                        found = true; // Resultado encontrado, parar a busca
-                    }
-                }
-            });
-
-            // Ir para a próxima página
-            currentPage++;
-
-            // Parar a busca se chegar ao fim dos resultados ou não encontrar correspondência
-            if ($('article').length === 0 || found) {
-                break;
+      // Função para buscar uma página e verificar se contém o termo de busca
+      async function fetchPageAndSearch(pageNumber) {
+         const response = await fetchFitGirlPage(pageNumber);
+         for (const post of response) {
+            if (post.title.toLowerCase().includes(searchTerm.toLowerCase())) {
+               results.push(post);
+               found = true;
+               break; // Para a busca se encontrar o termo
             }
-        }
+         }
+      }
 
-        console.log(`Resultados da busca pelo termo "${searchTerm}":`, results);
-        return results;
-    } catch (error) {
-        console.error('Erro ao buscar dados:', error.message);
-        return [];
-    }
+      const fetchPromises = [];
+      // Loop para buscar várias páginas até encontrar o termo ou atingir o limite
+      while (currentPage <= maxPages && !found) {
+         fetchPromises.push(fetchPageAndSearch(currentPage));
+         currentPage++;
+      }
+
+      // Espera todas as promessas resolverem, mas para se encontrado
+      await Promise.all(fetchPromises);
+
+      return results; // Retorna os resultados da busca
+   } catch (error) {
+      console.error('Erro ao buscar dados:', error.message);
+      return []; // Retorna um array vazio em caso de erro
+   }
 }
 
 // Rota para obter dados de uma página específica
 app.get('/', async (req, res) => {
-    const pageNumber = req.query.p; // Número da página a partir do parâmetro de consulta
-    if (pageNumber) {
-        const data = await fetchFitGirlPage(pageNumber);
-        if (data) {
-            res.json(data);
-        } else {
-            res.status(500).json({ error: 'Falha ao buscar dados' });
-        }
-    } else {
-        const searchTerm = req.query.q; // Termo de busca a partir do parâmetro de consulta
-        const data = await fetchFitGirlData(searchTerm);
-        if (data) {
-            res.json(data);
-        } else {
-            res.status(500).json({ error: 'Falha ao buscar dados' });
-        }
-    }
+   const pageNumber = req.query.p; // Número da página a partir do parâmetro de consulta
+   if (pageNumber) {
+      const data = await fetchFitGirlPage(pageNumber);
+      if (data) {
+         res.json(data); // Retorna os dados como JSON
+      } else {
+         res.status(500).json({ error: 'Falha ao buscar dados' }); // Retorna um erro HTTP 500 em caso de falha
+      }
+   } else {
+      const searchTerm = req.query.q; // Termo de busca a partir do parâmetro de consulta
+      const data = await fetchFitGirlData(searchTerm);
+      if (data) {
+         res.json(data); // Retorna os dados como JSON
+      } else {
+         res.status(500).json({ error: 'Falha ao buscar dados' }); // Retorna um erro HTTP 500 em caso de falha
+      }
+   }
 });
 
 // Iniciando o servidor
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+   console.log(`Servidor rodando na porta ${PORT}`); // Indica a porta onde o servidor está rodando
 });
